99219c0979eecec2b3e8cc2a27ac02297186849d
GoYutoPro <91126384+GoYutoPro@users.noreply.github.com>
2021-10-11 09:13:46 +0100

    Add files via upload

---
+++ products.js
@@ -0,0 +1,63 @@
+var ListOfProducts = {
+    Pet01: {
+        type: "Dog Bordeaux",
+        name: "Bobby",
+        breed: "dog de bordeaux",
+        description: "Newly born puppy",
+        image: "dummy-pdt-b.jpg",
+        price: 905
+    },
+
+    Pet02: {
+        type: "Dog Shepherds",
+        name: "Alex",
+        breed: "German Shepherd",
+        description: "3 weeks old dog",
+        image: "dummy-pdt-b.jpg",
+        price: 1200
+    },
+
+    Pet03: {
+        type: "Cat Persian",
+        name: "Melissa",
+        breed: "Persian Cat",
+        description: "8 weeks old cat",
+        image: "dummy-pdt-a.jpg",
+        price: 99.99
+    },
+
+    Pet04: {
+        type: "Cat Norwegian",
+        name: "Fiona",
+        breed: "Norwegian Forest cat",
+        description: "11 weeks old cat",
+        image: "dummy-pdt-a.jpg",
+        price: 110
+
+    },
+
+    Toy01: {
+        type: "Toy Rope",
+        description: "Dogs Rope Toy",
+        image: "Image of the Rope Toy",
+        price: 10.99
+
+    },
+
+    Toy02: {
+        type: "Toy Teaser",
+        description: "the Teaser Toy for cats",
+        image: "Image of the Teaser Toy",
+        price: 8
+
+    },
+
+    Toy03: {
+        type: "Cat Wheel Exercise",
+        description: "Cat Wheel toy for cats to exercise",
+        image: "Image of the Cat Wheel product",
+        price: 230
+
+    },
+
+}
